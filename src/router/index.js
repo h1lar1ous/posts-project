8220136cbd4c6b387f6022a161a8c634837881ca
h1lar1ous <romzaxh555@gmail.com>
2022-08-12 19:09:48 +0400

    firstcommit

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import PostIDPage from "../pages/PostIDPage";
+import Posts from "../pages/Posts";
+import About from "../pages/About";
+import Login from "../pages/Login";
+
+export const privateRoutes = [
+    {path: '/about', component: About, exact: true},
+    {path: '/posts', component: Posts, exact: true},
+    {path: '/posts/:id', component: PostIDPage, exact: true},
+]
+
+export const publicRoutes = [
+    {path: '/login', component: Login, exact: true}
+]
